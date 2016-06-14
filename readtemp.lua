abcacc31ae2c9462314997ec55ac36d963939eb5
RobVEsp <robvitters@outlook.com>
2016-06-14 15:36:39 +0000

    Add files via upload

---
+++ readtemp.lua
@@ -0,0 +1,9 @@
+pin = 4
+status, temp, humi, temp_dec, humi_dec = dht.read(pin)
+if status == dht.OK then
+    print("DHT Temperature:"..temp..";".."Humidity:"..humi)
+elseif status == dht.ERROR_CHECKSUM then
+    print( "DHT Checksum error." )
+elseif status == dht.ERROR_TIMEOUT then
+    print( "DHT timed out." )
+end
---
+++ setname.lua
@@ -0,0 +1,2 @@
+Name = "Buiten"
+Function ="TempOnly"
